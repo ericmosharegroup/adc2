248f9172bb530ed15f85bd4d619ccd436c7670c5
eric.mo <mozs@xiaopeng.com>
2018-06-30 11:02:09 +0800

    Add healthcheck

--- adc-boot/src/main/java/com/github/ericmoshare/adc/Application.java
+++ adc-boot/src/main/java/com/github/ericmoshare/adc/Application.java
@@ -2,12 +2,12 @@ package com.github.ericmoshare.adc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-//import com.alibaba.boot.dubbo.annotation.EnableDubboConfiguration;
 
 @SpringBootApplication
 @EnableScheduling
+@ImportResource("classpath*:spring-context.xml")
 public class Application {
 
     public static void main(String[] args) {
